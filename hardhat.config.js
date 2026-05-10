2c66e8d195a961447195547c4455fe8d9035248e
yaren1999 <demirciyarenmisra@gmail.com>
2026-05-10 14:12:47 +0300

    deploy: kontratlar Sepolia ağında yayına alındı
    - ArtToken, ArtNFT ve ArtMarketplace deploy edildi.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -12,4 +13,15 @@ module.exports = {
       },
     },
   },
+
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "", 
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [], 
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY, 
+  },
+
 };
